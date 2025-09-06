1ee0133de0e55de5fbe15a5e245843ef345a06ca
Felipe Augusto <felipea010203@gmail.com>
2025-09-06 10:39:36 -0300

    Update script.js
    Final version

--- script.js
+++ script.js
@@ -13,16 +13,31 @@ function configura_botoes_acess (_btn_ref, _pos_state, _pos_up, _pos_right, _wid
     _btn_ref.style.height = _height
 
     _btn_ref.style.display = _visibility
+    _btn_ref.style.zIndex = "9999"
 }
 
-function muda_tamanho_fonte (_p_queryAll, _st_queryAll, _fnt_b_s, _fnt_t_s) {
-        _p_queryAll.forEach(p => {
-            p.style.fontSize = _fnt_b_s + "px"
-        });
+function muda_fonte (_type, _p_queryAll, _st_queryAll, _param_1, _param_2) {
 
-        _st_queryAll.forEach(h3 => {
-            h3.style.fontSize = _fnt_t_s + "px"
-        });
+        switch (_type) {
+            case "tamanho":
+                _p_queryAll.forEach(p => {
+                    p.style.fontSize = _param_1 + "px"
+                });
+
+                _st_queryAll.forEach(h3 => {
+                    h3.style.fontSize = _param_2 + "px"
+                });
+                break;
+            case "contraste":
+                _p_queryAll.forEach(p => {
+                    p.style.color = _param_1
+                });
+
+                _st_queryAll.forEach(h3 => {
+                    h3.style.color = _param_2
+                });
+                break;
+        } 
 
 }
 
@@ -30,36 +45,84 @@ function muda_tamanho_fonte (_p_queryAll, _st_queryAll, _fnt_b_s, _fnt_t_s) {
 
 document.addEventListener("DOMContentLoaded", function () {
     let acessAtiva = false
+    let constrAtivo = false
+    let temaAtivo = "claro"
 
     const aumentaBotao = document.getElementById("aumenta-fonte")
     const diminuiBotao = document.getElementById("diminui-fonte")
+    const contrasteBotao = document.getElementById("faz-contraste")
+    const temaBotao = document.getElementById("muda-tema")
+
     const accesBotao = document.getElementById("ativa-acessibilidade")
 
-    const botoes_de_acess = [aumentaBotao, diminuiBotao]
     const texto_acessibilidade = document.createTextNode("  acessibilidade")
 
     const paragrafo = document.querySelectorAll("p")
     const subtitulos = document.querySelectorAll("h3")
 
+    configura_botoes_acess(accesBotao, "fixed", "50%", "10px", "auto", "auto", "block")
+
     configura_botoes_acess(aumentaBotao, "fixed", "120px", "20px", "250px", "auto", "none")
     configura_botoes_acess(diminuiBotao, "fixed", "160px", "20px", "250px", "auto", "none")
-    configura_botoes_acess(accesBotao, "fixed", "50%", "10px", "auto", "auto", "block")
+   
+    configura_botoes_acess(contrasteBotao, "fixed", "200px", "20px", "250px", "auto", "none")
+    configura_botoes_acess(temaBotao, "fixed", "240px", "20px", "250px", "auto", "none")
+
+    function mudaCores (_tema, _constr) {
+        var fundo_arcoIRis = document.getElementById("nosso-objetivo")
+        var galeria = document.getElementById("galeria-box")
+
+        if (_tema == "claro") {
+            document.body.style.backgroundColor = _constr ? "white" : "#f0f0f0"
+            galeria.style.backgroundColor = _constr ? "gray": "rgb(30, 255, 124)"
+            fundo_arcoIRis.style.backgroundColor = _constr ? "none" : "white"
+
+            if (_constr) {
+                muda_fonte("contraste", paragrafo, subtitulos, "black", "black")
+            } else {
+                muda_fonte("contraste", paragrafo, subtitulos, "rgb(51, 51, 51)", "rgb(86, 131, 3)")
+            } 
+        } else if (_tema == "escuro") {
+            document.body.style.backgroundColor = _constr ? "black" : "#202020ff"
+            galeria.style.backgroundColor = _constr ? "gray": "rgba(27, 0, 78, 1)"
+            fundo_arcoIRis.style.backgroundColor = _constr ? "none" : "black"
+
+            if (_constr) {
+                muda_fonte("contraste", paragrafo, subtitulos, "#FFFF00", "white")
+            } else {
+                muda_fonte("contraste", paragrafo, subtitulos, "rgba(216, 216, 216, 1)", "rgba(166, 255, 0, 1)")
+            } 
+        }
+    } 
 
     function criaBotaoFonte (_btn_ref, _soma) {
         _btn_ref.addEventListener("click", function () {
             font_size_basic += _soma
             font_size_subtitles += _soma
 
-            muda_tamanho_fonte(paragrafo, subtitulos, font_size_basic, font_size_subtitles)
+            muda_fonte("tamanho", paragrafo, subtitulos, font_size_basic, font_size_subtitles)
         })
-
     }
-
 
     criaBotaoFonte(aumentaBotao, 2)
     criaBotaoFonte(diminuiBotao, -2)
 
+    temaBotao.addEventListener("click", function () {
+        temaAtivo = temaAtivo == "claro" ? "escuro" : "claro"
+        
+
+        mudaCores(temaAtivo, constrAtivo)
+    })
+
+    contrasteBotao.addEventListener("click", function () {
+        constrAtivo = !constrAtivo
+        console.log(constrAtivo)
+        mudaCores(temaAtivo, constrAtivo)
+    })
+
+
     accesBotao.addEventListener("click", function () {
+        var _botoes = document.querySelectorAll(".acessibilidade")
         acessAtiva = !acessAtiva
 
         if (acessAtiva) {
@@ -73,8 +136,16 @@ document.addEventListener("DOMContentLoaded", function () {
             configura_botoes_acess(accesBotao, "fixed", "50%", "20px", "auto", "auto", "block")
         }
 
-        botoes_de_acess.forEach (_btn => {
+        _botoes.forEach (_btn => {
             _btn.style.display = acessAtiva ? "block" : "none";
         })        
+    })
+
+    anime ({
+        targets: ".show-up",
+        opacity: [0, 1],
+        duration: 1000,
+        easing: "easeInOutQuad",
+        translateY: [-50, 0]
     })
 })
